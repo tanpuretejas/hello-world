0e2d4182f0123e993d097ce2a7cd8cacb4559f84
tanpuretejas <tanpuretejas28@gmail.com>
2025-07-05 16:45:25 +0530

    Add files via upload
    added tejas intro file

---
+++ tejas_intro.py
@@ -0,0 +1,10 @@
+name=input("enter your name")
+goal=input("enter your goal")
+years=int(input("enter years in which you are going to complete your goal"))
+print("welcome",name)
+print("your goal is",goal)
+if years<=5:
+    print("you are achieving your goal in quick time")
+else:
+    print("long time but believe in consestency")
+    
